c57417985ecbcd2e615fec8a52505180e4409990
Pete Fullen <pfullen.code@gmail.com>
2017-12-21 14:24:41 -0500

    Implemented server post request for reservations

--- server.js
+++ server.js
@@ -8,11 +8,23 @@ var path = require("path");
 // Set up Express
 var app = express();
 var PORT = 3000;
-var reservations = [];
+var reservations = [{
+        name: "Peter",
+        phone: "404-931-8079",
+        email: "pfullen.code@gmail.com",
+        id: "pfullen",
+
+        status: "waiting" // waiting or table
+
+    }
+
+];
 
 
 // Sets up the express app to handle data
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 
@@ -20,50 +32,60 @@ app.use(bodyParser.json());
 // Routes
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "tables.html"));
+    res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 app.get("/reserve", function(req, res) {
-  res.sendFile(path.join(__dirname, "reserve.html"));
+    res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 
-//app.get()
 
-app.get("/api/tables", function (req, res){
-	res.json()
-})
-
-app.get("/api/reserve", function (req, res){
-	res.json(reservations);
+app.get("/api/tables", function(req, res) {
+    res.json(reservations);
 })
 
 
-/*
 
-app.post("/api/new", function (req, response) {
 
-  var newReservation = {
-  	name: "Peter",
-  	phone: "404-931-8079",
-  	email: "pfullen.code@gmail.com",
-  	id: "pfullen"
+app.post("/api/reserve", function(req, res) {
 
-  }   //req.body;
+    var newReservation = req.body;
 
-reservations.push(newReservation)
+    // newReservation = newReservation.replace(/\s+g, "");
 
-console.log(reservations);
+    /*{
+    	name: "Peter",
+    	phone: "404-931-8079",
+    	email: "pfullen.code@gmail.com",
+    	id: "pfullen"
+
+    }  */ //req.body;
+
+    // ck reg exp
+    
+      if (reservations.length < 5 ){
+      	  newReservation.status = "table";
+      } else {
+      	newReservation.status = "waiting";
+      }
+    
+    reservations.push(newReservation)
+    
+    console.log("This is the new reserv" + newReservation);
+    console.log(reservations);
+
+    res.json(newReservation);
 
 });
-*/
+
 
 // Starts the server to begin listening
 
 app.listen(PORT, function() {
-	console.log("App listening on PORT" + PORT);
-})
+    console.log("App listening on PORT" + PORT);
+});
